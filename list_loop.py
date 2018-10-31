de037493a58067c53a0bc0fd0e3738e043a31955
sedihglow <jamesmross4@gmail.com>
2018-10-30 19:29:31 -0700

    small exercises with loops

---
+++ list_loop.py
@@ -0,0 +1,16 @@
+names = ['alice', 'david', 'carolina']
+
+for name in names:
+    print(name.title() + " with a cat string")
+
+print("post loop")
+
+squares = []
+for value in range(1,11):
+    squares.append(value**2) # Store value squared into squares list
+
+print("\n\n")
+print(squares)
+
+
+
